cef30d1832b516a31c8825caaaf353bda8c5f840
Rammstein-1994 <lanc840930.mg09@nycu.edu.tw>
2023-03-05 19:37:37 +0800

    adding parameter

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,10 +1,18 @@
 from typing import Any
 
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.utilities.parser import event_parser, parse
 
 from src.handler.event_handler import DyproHandler
 
 
+class Parameters:
+    k1: float
+    k2: float
+    sample_size: int
+
+
+@event_parser(model=Parameters)
 def lambda_handler(event: dict[str, Any], context: LambdaContext) -> dict[str, Any]:
     event_hanlder = DyproHandler(event)
     return event_hanlder.handle()
