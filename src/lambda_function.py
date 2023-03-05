0a15dff5a5031f5ac63382568998ff4740c8e80a
Rammstein-1994 <lanc840930.mg09@nycu.edu.tw>
2023-03-05 19:41:32 +0800

    using lambda powertools

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,9 +1,12 @@
 from typing import Any
 
+from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from aws_lambda_powertools.utilities.parser import event_parser, parse
+from aws_lambda_powertools.utilities.parser import event_parser
 
 from src.handler.event_handler import DyproHandler
+
+logger = Logger()
 
 
 class Parameters:
@@ -13,6 +16,7 @@ class Parameters:
 
 
 @event_parser(model=Parameters)
+@logger.inject_lambda_context(log_event=True)
 def lambda_handler(event: dict[str, Any], context: LambdaContext) -> dict[str, Any]:
     event_hanlder = DyproHandler(event)
     return event_hanlder.handle()
